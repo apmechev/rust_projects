251a3167cfcbead0ce081ace8c7292c074fdd248
apmechev <apmechev@gmail.com>
2019-01-23 16:44:55 +0100

    adding basic variables

--- book_3_variables/src/main.rs
+++ book_3_variables/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5; 
+    println!("x is {}",x);
+    // x = 6; //this will give error E0384
+    //
+    let mut y = 5; 
+    println!("value of y is {}",y);
+    y=6;
+
+    println!("value of y is {}",y);
 }
