99c177446cef634cb19badc2308027f1170f4d88
apmechev <apmechev@gmail.com>
2019-01-23 16:55:38 +0100

    Adding examples on shadowing

--- book_3_variables/src/main.rs
+++ book_3_variables/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    ////////////////
+    //Mutable and Immutable values
+    ///////////////
     let x = 5; 
     println!("x is {}",x);
     // x = 6; //this will give error E0384
@@ -7,6 +9,39 @@ fn main() {
     let mut y = 5; 
     println!("value of y is {}",y);
     y=6;
-
+    
+   
+    y=7;
     println!("value of y is {}",y);
+    y=8;
+    println!("value of y is {}. we can keep updating this",y);
+
+    //////////////
+    //Overriding Keywords
+    //////////
+    let r#fn = "this variable is named 'fn' even though that's a keyword";
+    // can override identifiers
+    println!("{}",r#fn);
+
+    ////////////////
+    //Constants
+    ///////////////
+    const MAXY:u32 = 1_000_000;
+
+    //  MAXY=10; This will fail with E0070
+    
+    ////////////
+    //Shadowing
+    ////////////
+    let shad = 10;
+    let shad = shad + 1; //Keep in mind that 'let' here is a must
+    let shad = shad + 2;
+    //shad = shad+1; //This throws E0384
+    println!("The value of shad is {}",shad);
+
+    let shad: String = shad.to_string(); //We now made the integer into a string
+    // We can't do that with a variable; see below:
+    let mut mutshad = 5; 
+    // mutshad = "dd"; //This throws E0308
+
 }
